c30c6d87280645ab9e59668fc7d605b46bed4c73
esra <esrabaskaya84@gmail.com>
2025-08-06 22:01:38 +0200

    EventModifier

---
+++ EventModifier/script.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			counter: 0,
+			name: "",
+		};
+	},
+	methods: {
+		login() {
+			alert("Form submitted!");
+		},
+		setName(event, lastName) {
+			// debugger;
+			this.name = event.target.value + " " + lastName;
+		},
+		add(number) {
+			this.counter = this.counter + number;
+		},
+		reduce(number) {
+			this.counter = this.counter - number;
+		},
+	},
+});
+
+app.mount("#frontend");
