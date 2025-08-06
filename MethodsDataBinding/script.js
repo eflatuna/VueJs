9bef9ffe62a1773859140ce0885082a4c01196da
esra <esrabaskaya84@gmail.com>
2025-08-06 23:14:59 +0200

    `Deleted Vue.js project files and directories`

---
+++ MethodsDataBinding/script.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			counter: 0,
+			name: "",
+		};
+	},
+	methods: {
+		fullName() {
+			if (this.name === "") {
+				return "";
+			}
+			return this.name + " " + "Bas";
+		},
+		reset() {
+			this.name = "";
+		},
+		login() {
+			alert("Form submitted!");
+		},
+		setName(event) {
+			// debugger;
+			this.name = event.target.value;
+		},
+		add(number) {
+			this.counter = this.counter + number;
+		},
+		reduce(number) {
+			this.counter = this.counter - number;
+		},
+	},
+});
+
+app.mount("#frontend");
