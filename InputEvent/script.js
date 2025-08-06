e40db66c6967e9fc275f4cb456c63006b3185f32
esra <esrabaskaya84@gmail.com>
2025-08-06 21:38:10 +0200

    InputEvent

---
+++ InputEvent/script.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			counter: 0,
+			name: "",
+		};
+	},
+	methods: {
+		setName(event, lastName) {
+			debugger;
+			this.name = event.target.value + " " + lastName;
+		},
+		add(number) {
+			this.counter = this.counter + number;
+		},
+		reduce(number) {
+			this.counter = this.counter - number;
+		},
+	},
+});
+
+app.mount("#frontend");
