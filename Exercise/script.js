2fb37cd17dec225f29f79430452bda7aa9dd78b7
esra <esrabaskaya84@gmail.com>
2025-08-06 19:20:57 +0200

    `Update index.html and script.js to display personal information and image`

--- Exercise/script.js
+++ Exercise/script.js
@@ -1,20 +1,11 @@
 const app = Vue.createApp({
 	data() {
 		return {
-			firstGoal: "HTML",
-			secondGoal: "<h3>CSS</h3>",
-			website: "https://www.example.com",
+			name: "Esra",
+			age: "40",
+			imageUrl:
+				"https://images.unsplash.com/photo-1753735880239-d2213c79d1e4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
 		};
-	},
-	methods: {
-		ourGoal() {
-			const randomNumber = Math.random();
-			if (randomNumber < 0.4) {
-				return this.firstGoal;
-			} else {
-				return this.secondGoal;
-			}
-		},
 	},
 });
 
