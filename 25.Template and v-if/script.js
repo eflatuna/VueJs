a52f8d134c10defa34a01384f37f3794ed7982df
esra <esrabaskaya84@gmail.com>
2025-08-08 12:50:25 +0200

    Template and v-if

---
+++ 25.Template and v-if/script.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			skills: ["Html", "Css", "JavaScript", "VueJs"],
+		};
+	},
+	watch: {},
+	computed: {},
+	methods: {},
+});
+
+app.mount("#frontend");
