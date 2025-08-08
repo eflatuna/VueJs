d87610d7c7c2dda00de398d652dab9c03af60a83
esra <esrabaskaya84@gmail.com>
2025-08-08 20:45:40 +0200

    `Added health bars and attack functionality to the game, updated UI to display winner and controls`

--- 30.UygulamaTemplate/script.js
+++ 30.UygulamaTemplate/script.js
@@ -1,10 +1,43 @@
+function getRandomValue(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
 	data() {
-		return {};
+		return {
+			winner: null,
+			myHealth: 100,
+			computerHealth: 100,
+		};
 	},
 	watch: {},
-	computed: {},
-	methods: {},
+	computed: {
+		computerBarStyles() {
+			if (this.computerHealth < 0) {
+				return { width: "0%" };
+			} else {
+				return { width: this.computerHealth + "%" };
+			}
+		},
+		myBarStyles() {
+			if (this.myHealth < 0) {
+				return { width: "0%" };
+			} else {
+				return { width: this.myHealth + "%" };
+			}
+		},
+	},
+	methods: {
+		attackToComputer() {
+			const attackValue = getRandomValue(7, 15);
+			this.computerHealth = this.computerHealth - attackValue;
+			this.attackMe();
+		},
+		attackMe() {
+			const attackValue = getRandomValue(10, 20);
+			this.myHealth = this.myHealth - attackValue;
+		},
+	},
 });
 
 app.mount("#frontend");
