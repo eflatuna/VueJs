28d6e3454c55e9b221237ee38fc1f51703c79d6c
esra <esrabaskaya84@gmail.com>
2025-08-08 10:11:57 +0200

    Exercise

---
+++ 23.ComputedClass/script.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			firstBox: false,
+			secondBox: false,
+			thirdBox: false,
+		};
+	},
+	watch: {},
+	computed: {
+		boxOneSelected() {
+			return { selected: this.firstBox };
+		},
+	},
+	methods: {
+		chooseBox(number) {
+			if (number === "1") {
+				this.firstBox = true;
+			} else if (number === "2") {
+				this.secondBox = true;
+			} else if (number === "3") {
+				this.thirdBox = true;
+			}
+		},
+	},
+});
+
+app.mount("#frontend");
---
+++ 24.Exercise/script.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			inputBackGround: "red",
+			inputTextColor: "black",
+		};
+	},
+	watch: {},
+	computed: {},
+	methods: {},
+});
+
+app.mount("#frontend");
